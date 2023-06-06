a488065267d0c8dac1dd905b635e7ee28427ab4b
Vlad Aheiev <vageev06@gmail.com>
2023-06-06 18:26:20 -0400

    feat: added Header to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import "./globals.scss"
+import { Header } from "@/src/components/Header"
 import { Inter } from "next/font/google"
+
+import "./globals.scss"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
