25216c555992a845d721300da7f5ef6d54a3258c
Vlad Aheiev <vageev06@gmail.com>
2023-06-08 22:12:10 -0400

    feat: added pizza image to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image"
+import styles from "./homePage.module.scss"
 
 export default function Home() {
-  return <main></main>
+  return (
+    <main className={styles.main}>
+      <div className={styles.pizza_image_cropper}>
+        <div className={styles.pizza_image_wrapper}>
+          <Image fill src="/complete_pizza.png" alt="pepperoni pizza" />
+        </div>
+      </div>
+    </main>
+  )
 }
