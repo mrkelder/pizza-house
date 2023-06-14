dc9ad5b5904602f9be15f7966febac0a3a35746c
Vlad Aheiev <vageev06@gmail.com>
2023-06-13 21:37:27 -0400

    test: Button should accept native button attributes

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { Button } from "./Button"
 
 describe("Button component", () => {
@@ -7,5 +7,41 @@ describe("Button component", () => {
 
     render(<Button>{testText}</Button>)
     expect(screen.getByText(testText)).toBeInTheDocument()
+  })
+
+  it("should accept title, type, and role native button props", () => {
+    render(
+      <Button type="reset" title="Test title" role="button">
+        Text
+      </Button>
+    )
+
+    const buttonComponent = screen.getByText("Text")
+
+    expect(buttonComponent).toHaveAttribute("type", "reset")
+    expect(buttonComponent).toHaveAttribute("title", "Test title")
+    expect(buttonComponent).toHaveAttribute("role", "button")
+  })
+
+  it("should react to any native button DOM events", () => {
+    const mockClick = jest.fn()
+    const mockFocus = jest.fn()
+    const mockKeyDown = jest.fn()
+
+    render(
+      <Button onClick={mockClick} onFocus={mockFocus} onKeyDown={mockKeyDown}>
+        Text
+      </Button>
+    )
+
+    const buttonComponent = screen.getByText("Text")
+
+    fireEvent.click(buttonComponent)
+    fireEvent.focus(buttonComponent)
+    fireEvent.keyDown(buttonComponent)
+
+    expect(mockClick).toBeCalled()
+    expect(mockFocus).toBeCalled()
+    expect(mockKeyDown).toBeCalled()
   })
 })
