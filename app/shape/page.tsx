8cd485c8ea9cfdeefe864cf34a1b3941f3f58b7e
Vlad Aheiev <vageev06@gmail.com>
2023-06-16 13:52:15 -0400

    Revert "feat: start button redirects to /shape page"
    This reverts commit 5f728e261c19d0696c15f67c2124b8e8fb23e4f0.

--- app/shape/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./shapePage.module.scss"
-
-export default function Shape() {
-  return (
-    <main className={styles.main}>
-      <h1 className={styles.h1}>Select shape</h1>
-    </main>
-  )
-}
