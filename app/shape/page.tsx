c22deb25789e207ba39ad94b774685187a1250b8
Vlad Aheiev <vageev06@gmail.com>
2023-06-16 13:58:52 -0400

    feat: created /shape page

---
+++ app/shape/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./shapePage.module.scss"
+
+export default function Shape() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.h1}>Select shape</h1>
+    </main>
+  )
+}
