a67a287bae0fad980bd985a857ab9d0e1502cf1c
Vlad Aheiev <vageev06@gmail.com>
2023-06-19 10:03:15 -0400

    feat: added PizzaShape to page of shapes

--- app/shape/page.tsx
+++ app/shape/page.tsx
@@ -1,9 +1,14 @@
+import { PizzaShape } from "@/src/components/PizzaShape"
 import styles from "./shapePage.module.scss"
 
 export default function Shape() {
   return (
     <main className={styles.main}>
       <h1 className={styles.h1}>Select shape</h1>
+
+      <div className={styles.pizzaShapesGrid}>
+        <PizzaShape image="/complete_pizza.png" title="Circle" />
+      </div>
     </main>
   )
 }
