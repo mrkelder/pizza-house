ccd3129797fe7951eeac3f1461ba0ce84a80c072
Vlad Aheiev <vageev06@gmail.com>
2023-06-19 10:26:25 -0400

    test: corrected StringFormatter tests and added more situations

--- src/lib/StringFormatter/StringFormatter.test.ts
+++ src/lib/StringFormatter/StringFormatter.test.ts
@@ -9,13 +9,25 @@ describe("StringFormatter class", () => {
 
   it("should return page-shape-1 when enter '  PizZA   ShaPE   1  '", () => {
     expect(StringFormatter.getIdSlugFromString("  PizZA   ShaPE   1  ")).toBe(
-      "page-shape-1"
+      "pizza-shape-1"
     )
   })
 
   it("should return 1-2--345-shap-e when enter '1-2 -  - 3 4    5 SHAP E'", () => {
     expect(
       StringFormatter.getIdSlugFromString("1-2 -  - 3 4    5 SHAP E")
-    ).toBe("1-2--345-shap-e")
+    ).toBe("1-2-----3-4-5-shap-e")
+  })
+
+  it("should return https://www.google.com when enter 'https-www-google-com'", () => {
+    expect(StringFormatter.getIdSlugFromString("https://www.google.com")).toBe(
+      "https-www-google-com"
+    )
+  })
+
+  it("should return https://y.hoo/test?how=to&foo=bar when enter 'https-y-hoo-test-how-to-foo-bar'", () => {
+    expect(
+      StringFormatter.getIdSlugFromString("https://y.hoo/test?how=to&foo=bar")
+    ).toBe("https-y-hoo-test-how-to-foo-bar")
   })
 })
