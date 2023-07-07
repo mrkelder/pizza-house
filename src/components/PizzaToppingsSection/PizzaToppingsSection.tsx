2ff0d150fde3ef4a5770da8158daee8c3d58fc83
Vlad Aheiev <vageev06@gmail.com>
2023-07-07 19:30:24 -0400

    feat: created a toppings block to choose toppings

---
+++ src/components/PizzaToppingsSection/PizzaToppingsSection.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react"
+import styles from "./pizzaToppingsSection.module.scss"
+
+export const PizzaToppingsSection: FC = () => {
+  return <div className={styles.section}></div>
+}
