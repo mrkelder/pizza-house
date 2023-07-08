8efcb83fcb6a4520ccb85cdd5212380e8fd00c2d
Vlad Aheiev <vageev06@gmail.com>
2023-07-08 18:34:20 -0400

    feat: PizzaToppingsSection accepts toppings props

--- src/components/PizzaToppingsSection/PizzaToppingsSection.tsx
+++ src/components/PizzaToppingsSection/PizzaToppingsSection.tsx
@@ -1,6 +1,24 @@
 import { FC } from "react"
+import { PizzaTopping } from "@/src/lib/PizzaTopping"
 import styles from "./pizzaToppingsSection.module.scss"
 
-export const PizzaToppingsSection: FC = () => {
-  return <div className={styles.section}></div>
+interface PizzaToppingsSectionProps {
+  name: string
+  toppings: PizzaTopping[]
+}
+
+export const PizzaToppingsSection: FC<PizzaToppingsSectionProps> = ({
+  name,
+  toppings
+}) => {
+  return (
+    <div className={styles.section}>
+      <b>{name}</b>
+      {toppings.map((topping) => (
+        <div key={topping.id} data-id={topping.id}>
+          {topping.name}
+        </div>
+      ))}
+    </div>
+  )
 }
