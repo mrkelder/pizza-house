73b1324ad29dc5f884812c6b54b33dd3a0e9a2ea
Vlad Aheiev <vageev06@gmail.com>
2023-07-17 10:25:50 -0400

    fix: toppings use a method because Next.js 13 doesn't allow to use class instances

--- app/shape/[shapeId]/toppings.ts
+++ app/shape/[shapeId]/toppings.ts
@@ -5,17 +5,17 @@ export const meatToppings = [
     img: "/pizza_builder/bacon.webp",
     price: 0.56,
     name: "Bacon"
-  }),
+  }).getToppingObject(),
   new PizzaTopping({
     img: "/pizza_builder/chicken.webp",
     price: 0.16,
     name: "Chicken"
-  }),
+  }).getToppingObject(),
   new PizzaTopping({
     img: "/pizza_builder/pepperoni.webp",
     price: 1.48,
     name: "Pepperoni"
-  })
+  }).getToppingObject()
 ]
 
 export const vegetableToppings = [
@@ -23,27 +23,27 @@ export const vegetableToppings = [
     img: "/pizza_builder/peppers.png",
     price: 0.56,
     name: "Peppers"
-  }),
+  }).getToppingObject(),
   new PizzaTopping({
     img: "/pizza_builder/red_onions.png",
     price: 0.16,
     name: "Red Onions"
-  }),
+  }).getToppingObject(),
   new PizzaTopping({
     img: "/pizza_builder/tomatoes.png",
     price: 0.16,
     name: "Tomatoes"
-  }),
+  }).getToppingObject(),
   new PizzaTopping({
     img: "/pizza_builder/red_onions.png",
     price: 0.16,
     name: "Red Onions"
-  }),
+  }).getToppingObject(),
   new PizzaTopping({
     img: "/pizza_builder/tomatoes.png",
     price: 0.16,
     name: "Tomatoes"
-  })
+  }).getToppingObject()
 ]
 
 export const otherToppings = [
@@ -51,10 +51,10 @@ export const otherToppings = [
     img: "/pizza_builder/mushrooms.webp",
     price: 0.56,
     name: "Mushrooms"
-  }),
+  }).getToppingObject(),
   new PizzaTopping({
     img: "/pizza_builder/olives.png",
     price: 0.16,
     name: "Olives"
-  })
+  }).getToppingObject()
 ]
