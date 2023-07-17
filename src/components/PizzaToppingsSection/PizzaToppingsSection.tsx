10ce17fe30c9ec93344b83160dd5b0aa6e02145d
Vlad Aheiev <vageev06@gmail.com>
2023-07-17 09:45:13 -0400

    feat: toppings sections contains visual components

--- src/components/PizzaToppingsSection/PizzaToppingsSection.tsx
+++ src/components/PizzaToppingsSection/PizzaToppingsSection.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import Image from "next/image"
 import { PizzaTopping } from "@/src/lib/PizzaTopping"
 import styles from "./pizzaToppingsSection.module.scss"
 
@@ -14,11 +15,20 @@ export const PizzaToppingsSection: FC<PizzaToppingsSectionProps> = ({
   return (
     <div className={styles.section}>
       <b>{name}</b>
-      {toppings.map((topping) => (
-        <div key={topping.id} data-id={topping.id}>
-          {topping.name}
-        </div>
-      ))}
+      <div className={styles.toppingsList}>
+        {toppings.map((topping) => (
+          <div className={styles.topping} key={topping.id}>
+            <Image
+              src={topping.img}
+              width={40}
+              height={40}
+              alt={topping.name}
+            />
+            <p>{topping.name}</p>
+            <p>+ ${topping.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
