f228a758a86f9cd246db2b6eb9c33b42c82e9fb8
Vlad Aheiev <vageev06@gmail.com>
2023-07-31 20:22:33 -0400

    fix: replaceLayer is the only needed method

--- src/components/PizzaBuilderCanvas/useCanvasData.tsx
+++ src/components/PizzaBuilderCanvas/useCanvasData.tsx
@@ -6,17 +6,13 @@ import { useState } from "react"
 export const useCanvasData = () => {
   const [canvasObjects, setCanvasObjects] = useState<ToppingObject[]>([])
 
-  const addLayer = (topping: ToppingObject) => {
-    setCanvasObjects((prevData) => [...prevData, topping])
-  }
-
   const replaceLayer = (index: number, newTopping: ToppingObject) => {
-    setCanvasObjects((prevData) =>
-      prevData.map((prevObject, objectIndex) =>
-        index === objectIndex ? newTopping : prevObject
-      )
-    )
+    setCanvasObjects((prevData) => {
+      const prevDataCopy = Array.from(prevData)
+      prevDataCopy[index] = newTopping
+      return prevDataCopy
+    })
   }
 
-  return { canvasObjects, addLayer, replaceLayer }
+  return { canvasObjects, replaceLayer }
 }
